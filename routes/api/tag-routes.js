337ceaff02a59a79ed151b187e2a81ece09799f2
Madisen <madisenvogel@gmail.com>
2022-11-15 21:03:01 -0700

    tag routes tested and working

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -13,21 +13,44 @@ router.get('/', async (req, res) => {
   res.status(200).json(tagData);
 });
 
-router.get('/:id', (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
+router.get('/:id', async (req, res) => {
+  // find a single tag by its `id` and include associated Product data
+  const tagData = await Tag.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: [{ model: Product }],
+  }).catch((err => {
+    req.status(404).json(err);
+  }));
+  res.status(200).json(tagData);
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new tag
+  const tagData = await Tag.create(req.body).catch((err) => {
+    res.status(400).json(err);
+  });
+  res.status(200).json(tagData);
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
+  const tagData = await Tag.update({
+    tag_name: req.body.tag_name,
+  },
+  {
+    where: {
+      id: req.params.id,
+    },
+  }).catch((err) => {
+    res.status(400).json(err);
+  });
+  res.json(tagData);
 });
 
 router.delete('/:id', async (req, res) => {
-  // delete a category by its `id` value
+  // delete a tag by its `id` value
   const deletedTag = await Tag.destroy({
     where: {
       id: req.params.id
